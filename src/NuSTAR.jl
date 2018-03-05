13638fdb706604558a6815c36f87de157fd3e548
Robert Rosca <robert.rosca@protonmail.com>
2018-03-05 18:00:43 +0000

    Fix scratchless settings

--- src/NuSTAR.jl
+++ src/NuSTAR.jl
@@ -33,22 +33,24 @@ if !isfile(nustar_settings_dir)
     println("NU_ARCHIVE_CL: "); ENV["NU_ARCHIVE_CL"] = string(readline(STDIN))
     println("NU_ARCHIVE_PR: "); ENV["NU_ARCHIVE_PR"] = string(readline(STDIN))
 
-    println("NU_ARCHIVE_LIVE: ");    ENV["NU_ARCHIVE_LIVE"] = string(readline(STDIN))
-    println("NU_ARCHIVE_CL_LIVE: "); ENV["NU_ARCHIVE_CL_LIVE"] = string(readline(STDIN))
-    println("NU_ARCHIVE_PR_LIVE: "); ENV["NU_ARCHIVE_PR_LIVE"] = string(readline(STDIN))
-
-    println("NU_SCRATCH_FLAG (true/false): "); ENV["NU_SCRATCH_FLAG"] = parse(Bool, readline(STDIN))
-
     open(nustar_settings_dir, "w") do f
         write(f, "ENV[\"NU_ARCHIVE\"] = \"$(ENV["NU_ARCHIVE"])\"\n")
         write(f, "ENV[\"NU_ARCHIVE_CL\"] = \"$(ENV["NU_ARCHIVE_CL"])\"\n")
         write(f, "ENV[\"NU_ARCHIVE_PR\"] = \"$(ENV["NU_ARCHIVE_PR"])\"\n")
 
-        write(f, "ENV[\"NU_ARCHIVE_LIVE\"] = \"$(ENV["NU_ARCHIVE_LIVE"])\"\n")
-        write(f, "ENV[\"NU_ARCHIVE_CL_LIVE\"] = \"$(ENV["NU_ARCHIVE_CL_LIVE"])\"\n")
-        write(f, "ENV[\"NU_ARCHIVE_PR_LIVE\"] = \"$(ENV["NU_ARCHIVE_PR_LIVE"])\"\n")
-
         write(f, "ENV[\"NU_SCRATCH_FLAG\"] = $(ENV["NU_SCRATCH_FLAG"])\n")
+
+        println("NU_SCRATCH_FLAG (true/false): "); ENV["NU_SCRATCH_FLAG"] = parse(Bool, readline(STDIN))
+
+        if ENV["NU_SCRATCH_FLAG"] == "true"
+            println("NU_ARCHIVE_LIVE: ");    ENV["NU_ARCHIVE_LIVE"] = string(readline(STDIN))
+            println("NU_ARCHIVE_CL_LIVE: "); ENV["NU_ARCHIVE_CL_LIVE"] = string(readline(STDIN))
+            println("NU_ARCHIVE_PR_LIVE: "); ENV["NU_ARCHIVE_PR_LIVE"] = string(readline(STDIN))
+
+            write(f, "ENV[\"NU_ARCHIVE_LIVE\"] = \"$(ENV["NU_ARCHIVE_LIVE"])\"\n")
+            write(f, "ENV[\"NU_ARCHIVE_CL_LIVE\"] = \"$(ENV["NU_ARCHIVE_CL_LIVE"])\"\n")
+            write(f, "ENV[\"NU_ARCHIVE_PR_LIVE\"] = \"$(ENV["NU_ARCHIVE_PR_LIVE"])\"\n")
+        end
     end
 end
 
