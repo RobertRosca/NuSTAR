9022651eb993e44b1871734a34886bf633835a9a
Robert Rosca <robert.rosca@protonmail.com>
2018-03-05 18:07:36 +0000

    Make path for settings

--- src/NuSTAR.jl
+++ src/NuSTAR.jl
@@ -24,23 +24,25 @@ include("Analysis/FITSWCS.jl")
 include("Analysis/SourceDetect.jl")
 include("Analysis/Xselect.jl")
 
-nustar_settings_dir = string(homedir(), "/.config/julia-pkg/v0.6/NuSTAR/NuSTAR-settings.jl")
+nustar_settings_file = string(homedir(), "/.config/julia-pkg/v0.6/NuSTAR/NuSTAR-settings.jl")
 
-if !isfile(nustar_settings_dir)
-    warn("No settings file found at: $nustar_settings_dir\nEnter full paths for:\n")
+if !isfile(nustar_settings_file)
+    warn("No settings file found at: $nustar_settings_file\nEnter full paths for:\n")
+
+    mkpath(dirname(nustar_settings_file))
 
     println("NU_ARCHIVE: ");    ENV["NU_ARCHIVE"] = string(readline(STDIN))
     println("NU_ARCHIVE_CL: "); ENV["NU_ARCHIVE_CL"] = string(readline(STDIN))
     println("NU_ARCHIVE_PR: "); ENV["NU_ARCHIVE_PR"] = string(readline(STDIN))
 
-    open(nustar_settings_dir, "w") do f
+    println("NU_SCRATCH_FLAG (true/false): "); ENV["NU_SCRATCH_FLAG"] = parse(Bool, readline(STDIN))
+
+    open(nustar_settings_file, "w") do f
         write(f, "ENV[\"NU_ARCHIVE\"] = \"$(ENV["NU_ARCHIVE"])\"\n")
         write(f, "ENV[\"NU_ARCHIVE_CL\"] = \"$(ENV["NU_ARCHIVE_CL"])\"\n")
         write(f, "ENV[\"NU_ARCHIVE_PR\"] = \"$(ENV["NU_ARCHIVE_PR"])\"\n")
 
         write(f, "ENV[\"NU_SCRATCH_FLAG\"] = $(ENV["NU_SCRATCH_FLAG"])\n")
-
-        println("NU_SCRATCH_FLAG (true/false): "); ENV["NU_SCRATCH_FLAG"] = parse(Bool, readline(STDIN))
 
         if ENV["NU_SCRATCH_FLAG"] == "true"
             println("NU_ARCHIVE_LIVE: ");    ENV["NU_ARCHIVE_LIVE"] = string(readline(STDIN))
@@ -54,6 +56,6 @@ if !isfile(nustar_settings_dir)
     end
 end
 
-include(nustar_settings_dir)
+include(nustar_settings_file)
 
 end
