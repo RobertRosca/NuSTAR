789876a11c115d22fa66436e419c6876dcd3980c
Robert Rosca <robert.rosca@protonmail.com>
2018-03-07 01:55:18 +0000

    WebGen improvements

--- src/WebApp/WebGen.jl
+++ src/WebApp/WebGen.jl
@@ -4,6 +4,25 @@ function WebGen(;filename="/home/robertr/public_html/index.html", df="", select_
     end
 
     df = df[:, filter(x->!(x in blacklist_cols), names(df))]
+
+    gnr_c = 0
+    @from i in df begin
+        @where DateTime(i.public_date)<Base.Dates.today() && i.obs_type!="CAL" && i.observation_mode=="SCIENCE"
+        @select gnr_c += 1
+        @collect
+    end
+
+    df_summary = DataFrame(
+        Total=size(df, 1),
+        GoodPublic=gnr_c,
+        Downloaded=count(x->x==1, df[:Downloaded]),
+        Calibrated=count(x->x==1, df[:Cleaned]),
+        ValidScience=count(x->x==1, df[:ValidSci]),
+        RegSrcDone=count(x->x==1, df[:RegSrc]),
+        RegSrcIntr=count(x->x==2, df[:RegSrc]),
+        RegSrcBad=count(x->x==-1, df[:RegSrc]),
+        RegSrcCheck=count(x->x==-2, df[:RegSrc]))
+
     f = open(filename, "w")
 
     file_path = abspath(filename)
@@ -12,7 +31,7 @@ function WebGen(;filename="/home/robertr/public_html/index.html", df="", select_
     write(f, "<!DOCTYPE html>\n")
     write(f, "<html>\n")
     write(f, "<head>\n")
-    write(f, "\t <title>NuSTAR.jl WebView</title>\n")
+    write(f, "\t <title>NuSTAR.jl Web View</title>\n")
     write(f, "\t <meta charset=\"utf-8\">\n")
     write(f, "\t <link rel=\"stylesheet\" href=\"./assets/bootstrap/css/bootstrap.min.css\">\n")
     write(f, "\t <link rel=\"stylesheet\" href=\"./assets/bootstrap-table/src/bootstrap-table.css\">\n")
@@ -28,7 +47,53 @@ function WebGen(;filename="/home/robertr/public_html/index.html", df="", select_
     write(f, "<body>\n")
     write(f, "\t<div class=\"container\">\n")
     write(f, "\t<h1>NuSTAR.jl WebView</h1>\n")
-    write(f, "\t<p>Web view of current local Numaster table.</p>\n")
+    write(f, "\t<p>Table of the current local Numaster catalog, run `NuSTAR.Numaster()` to update</p>\n")
+    write(f, "\t<p>Mixing filters, search, and ordering, doesn't work too well currently. Doing basic things should work fine-ish though.</p>\n")
+    write(f, "\t<p>Page might take a while to fully load since the table is quite large.</p>\n")
+
+    write(f, "\t<hr>\n")
+
+    # Summary
+    write(f, "\t<h2>Summary</h2>\n")
+    write(f, "\t<table id=\"summary-table\"\n\t\t\t data-toggle=\"table\">\n")
+
+    write(f, "\t\t<thead>\n")
+    write(f, "\t\t\t<tr>\n")
+
+    cnames_summary = names(df_summary)
+
+    for column_name in cnames_summary
+        write(f, "\t\t\t\t<th >$column_name</th>\n")
+    end
+
+    write(f, "\t\t\t</tr>\n")
+    write(f, "\t\t</thead>\n")
+
+    write(f, "\t\t<tbody>\n")
+
+    n_summary = size(df_summary, 1)
+
+    mxrow_summary = n_summary
+
+    for row in 1:mxrow_summary
+        write(f, "\t\t\t<tr>\n")
+        for column_name in cnames_summary
+            cell = df_summary[row, column_name]
+            write(f, "\t\t\t\t<td>$(html_escape(cell))</td>\n")
+        end
+        write(f, "\t\t\t</tr>\n")
+    end
+    if n_summary > mxrow_summary
+        write(f, "\t\t\t<tr>\n")
+        write(f, "\t\t\t\t<th>&vellip;</th>\n")
+        for column_name in cnames_summary
+            write(f, "\t\t\t\t<td>&vellip;</td>\n")
+        end
+        write(f, "\t\t\t</tr>\n")
+    end
+    write(f, "\t\t</tbody>\n")
+    write(f, "\t</table>\n")
+
     write(f, "\t<hr>\n")
 
     # Table
