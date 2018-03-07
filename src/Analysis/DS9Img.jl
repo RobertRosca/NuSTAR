4978db2c77edf9be13376f2aa527102c15ed664f
Robert Rosca <robert.rosca@protonmail.com>
2018-03-07 23:43:56 +0000

    Skip if arrays are empty

--- src/Analysis/DS9Img.jl
+++ src/Analysis/DS9Img.jl
@@ -41,59 +41,65 @@ function ds9_make_img(;local_archive=ENV["NU_ARCHIVE"], local_archive_cl=ENV["NU
 
     f = open(ds9_queue_filename, "w")
 
-    for ds9_call in vcat(queue_uf, queue_cl)[1:10]
-        img_path = abspath(split(ds9_call)[end-1])
-        img_dir = dirname(abspath(split(ds9_call)[end-1]))
+    queue_cl_uf = vcat(queue_uf, queue_cl)
 
-        if !isdir(img_dir)
-            mkpath(img_dir)
-        end
+    if length(queue_cl_uf) > 0
+        for ds9_call in queue_cl_uf[1:10]
+            img_path = abspath(split(ds9_call)[end-1])
+            img_dir = dirname(abspath(split(ds9_call)[end-1]))
 
-        if isfile(img_path)
-            continue
-        end
+            if !isdir(img_dir)
+                mkpath(img_dir)
+            end
 
-        if debug
-            println("ds9 $ds9_call")
-        else
-            write(f, "ds9 $ds9_call\n")
-        end
-
-        info("Added to queue: $img_path")
-    end
-
-    for ds9_call in queue_cl_src[1:10] # Source-region images need to check the age of the regionfiles
-        img_path = abspath(split(ds9_call)[end-1])
-        img_dir  = dirname(abspath(split(ds9_call)[end-1]))
-        reg_src_path = abspath(split(ds9_call)[end-10])
-        reg_bkg_path = abspath(split(ds9_call)[end-8])
-
-        if !isdir(img_dir)
-            mkpath(img_dir)
-        end
-
-        if isfile(img_path)
-            reg_src_maketime = stat(reg_src_path).mtime
-            reg_bkg_maketime = stat(reg_bkg_path).mtime
-
-            reg_src_maketime >= reg_bkg_maketime? oldest = reg_bkg_maketime : oldest = reg_src_maketime
-
-            image_maketime = stat(img_path).mtime
-
-            if image_maketime - reg_src_maketime > 0 # image newer than source1
-                info("Image newer than source, skipped")
+            if isfile(img_path)
                 continue
             end
+
+            if debug
+                println("ds9 $ds9_call")
+            else
+                write(f, "ds9 $ds9_call\n")
+            end
+
+            info("Added to queue: $img_path")
         end
+    end
 
-        if debug
-            println("ds9 $ds9_call")
-        else
-            write(f, "ds9 $ds9_call\n")
+    if length(queue_cl_src) > 0
+        for ds9_call in queue_cl_src[1:10] # Source-region images need to check the age of the regionfiles
+            img_path = abspath(split(ds9_call)[end-1])
+            img_dir  = dirname(abspath(split(ds9_call)[end-1]))
+            reg_src_path = abspath(split(ds9_call)[end-10])
+            reg_bkg_path = abspath(split(ds9_call)[end-8])
+
+            if !isdir(img_dir)
+                mkpath(img_dir)
+            end
+
+            if isfile(img_path)
+                reg_src_maketime = stat(reg_src_path).mtime
+                reg_bkg_maketime = stat(reg_bkg_path).mtime
+
+                reg_src_maketime >= reg_bkg_maketime? oldest = reg_bkg_maketime : oldest = reg_src_maketime
+
+                image_maketime = stat(img_path).mtime
+
+                if image_maketime - reg_src_maketime > 0 # image newer than source1
+                    info("Image newer than source, skipped")
+                    continue
+                end
+            end
+
+            if debug
+                println("ds9 $ds9_call")
+            else
+                write(f, "ds9 $ds9_call\n")
+            end
+
+            close(f)
+
+            info("Added to queue: $img_path")
         end
-
-        close(f)
-
-        info("Added to queue: $img_path")
     end
 end
