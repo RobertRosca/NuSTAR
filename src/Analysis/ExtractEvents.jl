9c7b8a7a9f1405b9f4000c60dc6e4cfaac659679
Robert Rosca <robert.rosca@protonmail.com>
2018-03-24 12:18:52 +0000

    Debugging code, not needed, commited for reference

--- src/Analysis/ExtractEvents.jl
+++ src/Analysis/ExtractEvents.jl
@@ -10,8 +10,8 @@ struct Binned_event
     obsid::String
     typeof::String
     bin::Number
-    counts::SparseVector{Int64,Int64}
-    time_edges::StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}}
+    counts::SparseVector
+    time_edges::StepRangeLen
     gtis::Array{UnitRange{Int64},1}
 end
 
@@ -53,7 +53,7 @@ end
 function extract_evts(evt_path; gti_width_min::Number=128)
     evt_file = FITS(evt_path)
     evt_obsid = read_key(evt_file[1], "OBS_ID")[1]
-    evt_time_start = read_key(evt_file[1], "TSTART")[1] + 0.5 # These are off by 1/2 sec, for some reason...
+    evt_time_start = read_key(evt_file[1], "TSTART")[1]
     evt_time_stop  = read_key(evt_file[1], "TSTOP")[1]
     evt_time_elapse = read_key(evt_file[1], "TELAPSE")[1]
 
@@ -74,23 +74,120 @@ function extract_evts(evt_path; gti_width_min::Number=128)
     return Unbinned_event(evt_obsid, evt_events, evt_gtis, evt_time_stop, evt_time_start)
 end
 
+function extract_evts2(evt_path; gti_width_min::Number=128)
+    evt_file = FITS(evt_path)
+    evt_obsid = read_key(evt_file[2], "OBS_ID")[1]
+    evt_time_start = read_key(evt_file[2], "TSTART")[1]
+    evt_time_stop  = read_key(evt_file[2], "TSTOP")[1]
+    evt_time_elapse = read_key(evt_file[2], "TELAPSE")[1]
+
+    time = read(evt_file[2], "TIME") .- evt_time_start
+
+    evt_events = DataFrame(TIME=time)
+
+    evt_gti = DataFrame(START=read(evt_file[3], "START").-evt_time_start, STOP=read(evt_file[3], "STOP").-evt_time_start)
+    evt_gti[:WIDTH] = evt_gti[:STOP] .- evt_gti[:START] # GTI interval width in seconds
+    evt_gti[:GOOD]  = evt_gti[:WIDTH] .>= gti_width_min;
+
+    # Create tuple of GTI start and stop times, [sec] since evt_time_start
+    evt_gti = @from gti in evt_gti begin
+        @where gti.GOOD == 1
+        @select [gti.START, gti.STOP]
+        @collect
+    end
+
+    return Unbinned_event(evt_obsid, evt_events, evt_gti, evt_time_stop, evt_time_start)
+end
+
+function extract_lc2(evt_path; gti_width_min::Number=128) # extract_lc2
+    evt_file = FITS(evt_path)
+    evt_obsid = read_key(evt_file[2], "OBS_ID")[1]
+    evt_time_start = read_key(evt_file[2], "TSTART")[1]
+    evt_time_stop  = read_key(evt_file[2], "TSTOP")[1]
+    evt_time_elapse = read_key(evt_file[2], "TELAPSE")[1]
+
+    rate = read(evt_file[2], "RATE")
+    time = read(evt_file[2], "TIME")
+    time = time[rate.!=0]
+
+    evt_events = DataFrame(TIME=time)
+
+    evt_gti = DataFrame(START=read(evt_file[3], "START").-evt_time_start, STOP=read(evt_file[3], "STOP").-evt_time_start)
+    evt_gti[:WIDTH] = evt_gti[:STOP] .- evt_gti[:START] # GTI interval width in seconds
+    evt_gti[:GOOD]  = evt_gti[:WIDTH] .>= gti_width_min;
+
+    # Create tuple of GTI start and stop times, [sec] since evt_time_start
+    evt_gti = @from gti in evt_gti begin
+        @where gti.GOOD == 1
+        @select [gti.START, gti.STOP]
+        @collect
+    end
+
+    return Unbinned_event(evt_obsid, evt_events, evt_gti, evt_time_stop, evt_time_start)
+end
+
+
+function extract_lc(evt_path; gti_width_min::Number=128)
+    evt_file = FITS(evt_path)
+    evt_obsid = read_key(evt_file[2], "OBS_ID")[1]
+    evt_time_start = read_key(evt_file[2], "TSTART")[1]
+    evt_time_stop  = read_key(evt_file[2], "TSTOP")[1]
+    evt_time_elapse = read_key(evt_file[2], "TELAPSE")[1]
+    bin_sec = read_key(evt_file[2], "TIMEDEL")[1]
+
+    evt_time_edges = 0:round(bin_sec, 3):round(evt_time_elapse, 3)
+
+    info("Creating time index")
+
+    time_as_index = round.(Int, read(evt_file[2], "TIME")./(2e-3)) .+ 1
+
+    info("Getting fits rates")
+
+    fits_rate = read(evt_file[2], "RATE")
+
+    info("Creating sparse array")
+
+    evt_counts = spzeros(length(evt_time_edges)+1)
+
+    info("Writing rates to sparray")
+
+    evt_counts[time_as_index] = fits_rate
+
+    return evt_counts, evt_time_edges
+
+    evt_events = DataFrame(TIME=read(evt_file[2], "TIME"), RATE=read(evt_file[2], "RATE"))
+
+
+    evt_gti = DataFrame(START=read(evt_file[3], "START").-evt_time_start, STOP=read(evt_file[3], "STOP").-evt_time_start)
+
+    evt_gti[:WIDTH] = evt_gti[:STOP] .- evt_gti[:START] # GTI interval width in seconds
+    evt_gti[:GOOD]  = evt_gti[:WIDTH] .>= gti_width_min;
+
+    sparse_rate = sparse(evt_events[:RATE])
+
+    return evt_events, evt_gti, evt_time_edges
+
+    #Unbinned_event(evt_obsid, evt_events, evt_gtis, evt_time_stop, evt_time_start)
+    return Binned_event(evt_obsid, "lc", bin_sec, sparse_rate, evt_time_edges, gtis)
+end
+
 function bin_evts_lc(bin_sec, unbinned)
     if bin_sec < 2e-3
         error("NuSTAR temportal resolution is 2e-3, cannot bin under that value, binsec $bin_sec is invalid")
     end
 
-    evt_time_edges = bin_sec:bin_sec:(unbinned.stop-unbinned.start) # Construct edges for histogram, finish at stop time (w.r.t. obs start)
+    evt_time_edges = 0:bin_sec:(unbinned.stop-unbinned.start) # Construct edges for histogram, finish at stop time (w.r.t. obs start)
 
     gti_intervals = size(unbinned.gtis, 1)
     evt_gtis = hcat(unbinned.gtis...)' # Convert to matrix
-    evt_gtis = round.(evt_gtis, 3) # Fix floating point errors
+    evt_gtis = evt_gtis # Fix floating point errors
 
     gtis = map(x->findfirst(evt_time_edges.>=x), evt_gtis) .- [zeros(Int, gti_intervals) ones(Int, gti_intervals)] # Subtract one from the GTI end bins
     if gtis[end] == -1; gtis[end] = length(evt_time_edges); end # Fix for GTI at end of time
     gtis = range.(gtis[:, 1], gtis[:, 2].-gtis[:, 1])
 
     evt_counts = begin
-        hist_binning = fit(Histogram, unbinned.event[:TIME], evt_time_edges, closed=:right)
+        hist_binning = fit(Histogram, unbinned.event[:TIME], evt_time_edges, closed=:left)
         sparse(hist_binning.weights) # Perform histogram fit, return sparse vector to save on computation
     end
 
