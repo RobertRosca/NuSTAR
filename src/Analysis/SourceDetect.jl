62bd658cdb5a7cc66c891012de360ee4cc0a5d99
Robert Rosca <robert.rosca@protonmail.com>
2018-04-01 16:55:54 +0100

    Fix source detect cd before ds9

--- src/Analysis/SourceDetect.jl
+++ src/Analysis/SourceDetect.jl
@@ -155,7 +155,7 @@ function MakeSourceReg(path; skip_bad=false)
             return
         end
 
-        cd(dirname(string(path, "/..")))
+        cd(dirname(string(path)), "/..")
 
         command = `ds9 $path -regions $source_reg_file_unchecked`
 
