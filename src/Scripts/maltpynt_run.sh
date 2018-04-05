2abbd614ced6f8cdfa32d44c35137f053e1b3542
Robert Rosca <robert.rosca@protonmail.com>
2018-04-05 22:58:31 +0100

    Change binsize back to RMS of 0.002

--- src/Scripts/maltpynt_run.sh
+++ src/Scripts/maltpynt_run.sh
@@ -97,7 +97,7 @@ do
 	path_b_calib="${path_mp}nu${ObsID}B01_ev_calib.p"
 
 	echo "${highlight}MPlcurve ${reset}"
-	MPlcurve $path_a_calib $path_b_calib -b 0.001 -e 3 30 --safe-interval 100 300
+	MPlcurve $path_a_calib $path_b_calib -b 0.002 -e 3 30 --safe-interval 100 300
 
 	path_a_lc="${path_mp}nu${ObsID}A01_E3-30_lc.p"
 	path_b_lc="${path_mp}nu${ObsID}B01_E3-30_lc.p"
@@ -105,7 +105,7 @@ do
 	echo "${highlight}MPfspec ${reset}"
 	MPfspec $path_a_lc $path_b_lc -k CPDS -o leahy --norm Leahy
 
-	path_rms_cpds="${path_mp}rms_cpds.p"
+	path_cpds="${path_mp}leahy_cpds.p"
 
-	$SCRIPT_DIR/pickle2hdf5 $path_rms_cpds
+	$SCRIPT_DIR/pickle2hdf5 $path_cpds
 done
