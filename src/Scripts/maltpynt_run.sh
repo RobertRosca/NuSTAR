cb7de5709f484f83d565d150063084cd18b96409
Robert Rosca <robert.rosca@protonmail.com>
2018-04-05 18:47:57 +0100

    Improved maltpynt call and pickle conversion

--- src/Scripts/maltpynt_run.sh
+++ src/Scripts/maltpynt_run.sh
@@ -1,35 +1,71 @@
 #!/bin/bash
 
-nu_archive_cl="/home/robert/Scratch/.nustar_archive_cl/"
-obsid="90302319006"
-path_pipeline="$nu_archive_cl$obsid/pipeline_out/"
-path_mp="${path_pipeline}MP/"
+# http://www.bahmanm.com/blogs/command-line-options-how-to-parse-in-bash-using-getopt
 
-path_a="${path_pipeline}nu${obsid}A01_cl.evt"
-path_b="${path_pipeline}nu${obsid}B01_cl.evt"
+# read the options
+TEMP=`getopt -o c:o: --long clean:,obsids: -n 'run_native_nupipeline.sh' -- "$@"`
+eval set -- "$TEMP"
 
-MPreadevents $path_a $path_b
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-path_a_ev="${path_mp}nu${obsid}A01_ev.p"
-path_b_ev="${path_mp}nu${obsid}B01_ev.p"
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+        c|--clean)
+            case "$2" in
+                "") shift 2 ;;
+                *) clean=$2 ; shift 2 ;;
+            esac ;;
+        o|--obsids)
+            case "$2" in
+                "") shift 2 ;;
+                *) obsids=$2 ; shift 2 ;;
+            esac ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
 
-mkdir $path_mp
+obsids=( $obsids )
 
-mv "${path_pipeline}nu${obsid}A01_cl_ev.p" $path_a_ev
-mv "${path_pipeline}nu${obsid}B01_cl_ev.p" $path_b_ev
+highlight=`tput setaf 6`
+reset=`tput sgr0`
 
-MPcalibrate $path_a_ev $path_b_ev
+echo "${highlight}Running for ${obsids[@]} ${reset}"
 
-path_a_calib="${path_mp}nu${obsid}A01_ev_calib.p"
-path_b_calib="${path_mp}nu${obsid}B01_ev_calib.p"
+for ObsID in "${obsids[@]}"
+do
+	path_pipeline="$clean$ObsID/pipeline_out/"
+	path_mp="${path_pipeline}MP0/"
 
-MPlcurve $path_a_calib $path_b_calib -b 0.002 -e 3 30 --safe-interval 100 300
+	path_a="${path_pipeline}nu${ObsID}A01_cl.evt"
+	path_b="${path_pipeline}nu${ObsID}B01_cl.evt"
 
-path_a_lc="${path_mp}nu${obsid}A01_E3-30_lc.p"
-path_b_lc="${path_mp}nu${obsid}B01_E3-30_lc.p"
+	MPreadevents $path_a $path_b
 
-MPfspec $path_a_lc $path_b_lc -k CPDS -o rms --norm rms
+	path_a_ev="${path_mp}nu${ObsID}A01_ev.p"
+	path_b_ev="${path_mp}nu${ObsID}B01_ev.p"
 
-path_rms_cpds="${path_mp}rms_cpds.p"
+    if [ ! -d "$path_mp" ]; then
+        mkdir $path_mp
+    fi
 
-MP2xspec $path_rms_cpds
+	mv "${path_pipeline}nu${ObsID}A01_cl_ev.p" $path_a_ev
+	mv "${path_pipeline}nu${ObsID}B01_cl_ev.p" $path_b_ev
+
+	MPcalibrate $path_a_ev $path_b_ev
+
+	path_a_calib="${path_mp}nu${ObsID}A01_ev_calib.p"
+	path_b_calib="${path_mp}nu${ObsID}B01_ev_calib.p"
+
+	MPlcurve $path_a_calib $path_b_calib -b 0.0002 -e 3 30 --safe-interval 100 300
+
+	path_a_lc="${path_mp}nu${ObsID}A01_E3-30_lc.p"
+	path_b_lc="${path_mp}nu${ObsID}B01_E3-30_lc.p"
+
+	MPfspec $path_a_lc $path_b_lc -k CPDS -o rms --norm rms
+
+	path_rms_cpds="${path_mp}rms_cpds.p"
+
+	$SCRIPT_DIR/pickle2hdf5 $path_rms_cpds
+done
