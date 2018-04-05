d14ce89489ff3b2f9b9b12359875363476f05fd5
Robert Rosca <robert.rosca@protonmail.com>
2018-04-05 19:20:11 +0100

    Fix echo typo

--- src/Scripts/maltpynt_run.sh
+++ src/Scripts/maltpynt_run.sh
@@ -41,7 +41,7 @@ do
 	path_a="${path_pipeline}nu${ObsID}A01_cl.evt"
 	path_b="${path_pipeline}nu${ObsID}B01_cl.evt"
 
-	echo "${highlight}MPreadevents${obsids[@]} ${reset}"
+	echo "${highlight}MPreadevents ${reset}"
 	MPreadevents $path_a $path_b
 
 	path_a_ev="${path_mp}nu${ObsID}A01_ev.p"
@@ -54,19 +54,19 @@ do
 	mv "${path_pipeline}nu${ObsID}A01_cl_ev.p" $path_a_ev
 	mv "${path_pipeline}nu${ObsID}B01_cl_ev.p" $path_b_ev
 
-	echo "${highlight}MPcalibrate${obsids[@]} ${reset}"
+	echo "${highlight}MPcalibrate ${reset}"
 	MPcalibrate $path_a_ev $path_b_ev
 
 	path_a_calib="${path_mp}nu${ObsID}A01_ev_calib.p"
 	path_b_calib="${path_mp}nu${ObsID}B01_ev_calib.p"
 
-	echo "${highlight}MPlcurve${obsids[@]} ${reset}"
+	echo "${highlight}MPlcurve ${reset}"
 	MPlcurve $path_a_calib $path_b_calib -b 0.0002 -e 3 30 --safe-interval 100 300
 
 	path_a_lc="${path_mp}nu${ObsID}A01_E3-30_lc.p"
 	path_b_lc="${path_mp}nu${ObsID}B01_E3-30_lc.p"
 
-	echo "${highlight}MPfspec${obsids[@]} ${reset}"
+	echo "${highlight}MPfspec ${reset}"
 	MPfspec $path_a_lc $path_b_lc -k CPDS -o leahy --norm leahy
 
 	path_rms_cpds="${path_mp}rms_cpds.p"
