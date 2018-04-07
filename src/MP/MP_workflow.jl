1aba53aa152a374ac129d039ad0bdd506cb03689
Robert Rosca <robert.rosca@protonmail.com>
2018-04-07 01:26:40 +0100

    Add multithreaded batch

--- src/MP/MP_workflow.jl
+++ src/MP/MP_workflow.jl
@@ -1,4 +1,34 @@
-function MP_products(obsid; bintime = 2e-3, minimum_count_rate = 2, clobber=false,
+function MP_calib_total_crate(data_calib::MP_calib; min_gti_sec=512)
+    gti_good = data_calib.GTI[([x[2]-x[1] for x in data_calib.GTI] .> 512)]
+
+    count_evt  = 0
+    count_time = 0
+    for gti in gti_good
+        count_evt  += count(gti[2] .> data_calib.time .> gti[1])
+        count_time += gti[2] - gti[1]
+    end
+
+    count_rate = count_evt/count_time
+
+    return count_rate
+end
+
+function MP_calib_total_crate(data_a_calib::MP_calib, data_b_calib::MP_calib; min_gti_sec=512)
+    count_rate_a = MP_calib_total_crate(data_a_calib; min_gti_sec=min_gti_sec)
+    count_rate_b = MP_calib_total_crate(data_b_calib; min_gti_sec=min_gti_sec)
+
+    return count_rate_a + count_rate_b
+end
+
+function MP_calib_total_crate(path_a_calib::String, path_b_calib::String; min_gti_sec=512)
+    data_a_calib = MP_parse_calib(path_a_calib)
+    data_b_calib = MP_parse_calib(path_b_calib)
+
+    return MP_calib_total_crate(data_a_calib, data_b_calib; min_gti_sec=min_gti_sec)
+end
+
+
+function MP_produce_lc(obsid; bintime = 2e-3, minimum_count_rate = 2, clobber=false,
         local_archive_cl=ENV["NU_ARCHIVE_CL"], local_archive_pr=ENV["NU_ARCHIVE_PR"])
 
     path_pipeline = string(local_archive_cl, obsid, "/pipeline_out/")
@@ -53,7 +83,7 @@ function MP_products(obsid; bintime = 2e-3, minimum_count_rate = 2, clobber=fals
     path_b_lc = string(path_mp_out, "nu$(obsid)B01_cl_lc.p")
 
     if isfile(path_a_lc) && isfile(path_b_lc) && !clobber
-        info("Calib files already exist, skipping - $path_a_ev")
+        info("Lightcurve files already exist, skipping - $path_a_ev")
     else
         total_count_rate = MP_calib_total_crate(path_a_calib, path_b_calib)
 
@@ -67,35 +97,32 @@ function MP_products(obsid; bintime = 2e-3, minimum_count_rate = 2, clobber=fals
         info("MPlcurve - $path_b_calib")
         maltpynt.lcurve[:lcurve_from_events](path_b_calib, safe_interval=[100, 300], bintime=bintime)
     end
-
-    @assert isfile(path_a_lc) && isfile(path_b_lc) "MP lc files not found, error while generating?"
 end
 
-function MP_calib_total_crate(data_calib::MP_calib; min_gti_sec=512)
-    gti_good = data_calib.GTI[([x[2]-x[1] for x in data_calib.GTI] .> 512)]
+function MP_produce_lc_batch(;local_archive=ENV["NU_ARCHIVE"], local_archive_cl=ENV["NU_ARCHIVE_CL"],
+                   local_utility=ENV["NU_ARCHIVE_UTIL"], procs_to_use=4, todo=16, dry=false)
 
-    count_evt  = 0
-    count_time = 0
-    for gti in gti_good
-        count_evt  += count(gti[2] .> data_calib.time .> gti[1])
-        count_time += gti[2] - gti[1]
+   numaster_path = string(local_utility, "/numaster_df.csv")
+
+   numaster_df = read_numaster(numaster_path)
+
+   queue = @from i in numaster_df begin
+       @where i.RegSrc==1 && i.MP==0
+       @select i.obsid
+       @collect
+   end
+
+   if length(queue) > todo
+       queue = queue[1:todo]
+   end
+
+   if nprocs() < procs_to_use
+        addprocs(procs_to_use - nprocs())
     end
 
-    count_rate = count_evt/count_time
+    info("Using $(nprocs()) procs")
 
-    return count_rate
-end
-
-function MP_calib_total_crate(data_a_calib::MP_calib, data_b_calib::MP_calib; min_gti_sec=512)
-    count_rate_a = MP_calib_total_crate(data_a_calib; min_gti_sec=min_gti_sec)
-    count_rate_b = MP_calib_total_crate(data_b_calib; min_gti_sec=min_gti_sec)
-
-    return count_rate_a + count_rate_b
-end
-
-function MP_calib_total_crate(path_a_calib::String, path_b_calib::String; min_gti_sec=512)
-    data_a_calib = MP_parse_calib(path_a_calib)
-    data_b_calib = MP_parse_calib(path_b_calib)
-
-    return MP_calib_total_crate(data_a_calib, data_b_calib; min_gti_sec=min_gti_sec)
+    @parallel for obsid in queue
+        MP_calib_total_crate(obsid)
+    end
 end
